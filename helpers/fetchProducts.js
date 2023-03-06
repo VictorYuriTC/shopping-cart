f300af7638430d8dbe64a74c3c18ded7cdc8dec8
Victor Yuri <victoryuritc@yahoo.com>
2023-03-06 19:13:39 -0300

    fix: remove remaining tests

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -2,7 +2,7 @@ const fetchProducts = async (productToBeSearched) => {
   try {
     const productUrl = `https://api.mercadolibre.com/sites/MLB/search?q=${productToBeSearched}`;
     const productResponse = await fetch(productUrl);
-    const productsData = await productResponse.json();
+    const productsData = await productResponse.json();z
     return productsData;
   } catch (error) {
     return error;
